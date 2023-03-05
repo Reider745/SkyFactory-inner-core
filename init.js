2647d109a2d94c810467edf03ce7779a897a0cfc
Reider745 <fhgfdf574@gmail.com>
2023-03-05 17:30:50 +0300

    Удалил сообщение для Артёма

--- init.js
+++ init.js
@@ -6,4 +6,3 @@
     if(MAIN_FILE.information.version_icmods !== undefined && Number(FileTools.ReadJSON(__modpack__.getRootDirectoryPath()+"/preferences.json").icmods_version) != 26)
         self.parseDialog(MAIN_FILE.information.dialog_type, self.getFile(MAIN_FILE.information.change_logs[MAIN_FILE.information.version_icmods]));
 }catch(e){alert("Пожалуйста сообщите разработчику Sky Factory об ошибке: "+e+"\nP.S Ошибка не критична")}*/
-self.parseDialog(null, "<text>:Артём где Двери?", "Сообщение для Артёма");
